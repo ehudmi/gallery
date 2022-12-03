ef884b11f9044688d978b9f966584660d7c181f6
Mika Bibas <mikabibas@gmail.com>
2022-12-03 11:04:01 +0200

    design welcome

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -1,13 +1,20 @@
 import { useNavigate } from "react-router-dom";
+import styles from "./styles/Welcome.module.css";
 
 function Welcome() {
   const navigate = useNavigate();
   return (
-    <div>
-      <div>Welcome to the gallery of projects</div>
-      <button onClick={() => navigate("/signup")}>Signup</button>
-      <button onClick={() => navigate("/login")}>Login</button>
-    </div>
+    <header className={styles.container}>
+      {/* <div className={styles.welcome}>Welcome to the gallery of projects</div> */}
+      <div className={styles.btnContainer}>
+        <button className={styles.button20} onClick={() => navigate("/signup")}>
+          Signup
+        </button>
+        <button className={styles.button20} onClick={() => navigate("/login")}>
+          Login
+        </button>
+      </div>
+    </header>
   );
 }
 
