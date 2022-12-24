ee52b6efc7fe2d0631341468a01e43e86e5054b6
ehud <ehudmi12@gmail.com>
2022-12-24 10:45:37 +0200

    minor

--- client/src/containers/ProjectsList.js
+++ client/src/containers/ProjectsList.js
@@ -56,7 +56,7 @@ function ProjectsList() {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          id: [id],
+          project_id: [id],
         }),
       });
       const json = await response.json();
