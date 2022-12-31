474ec49f38c8aaf299ab2a6f3d3f7e076d26e683
ehud <ehudmi12@gmail.com>
2022-12-31 21:03:20 +0200

    updated server.js

--- server.js
+++ server.js
@@ -14,14 +14,14 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 
+// app.use("/", express.static(path.resolve(__dirname, "./client/build")));
+
 app.use("/users", require("./routes/user.routes"));
 app.use("/projects", require("./routes/project.routes"));
 
-app.use("/", express.static(path.resolve(__dirname, "./client/build")));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
-
 app.listen(process.env.PORT || 8080, () => {
   console.log(`server is running on port ${process.env.PORT || 8080}`);
 });
