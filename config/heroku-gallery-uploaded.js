3c5ef0a62f4ef1a4dc34357e8328e82ca208ca27
ehud <ehudmi12@gmail.com>
2023-03-10 14:26:39 +0200

    update url_db

--- config/heroku-gallery-uploaded.js
+++ config/heroku-gallery-uploaded.js
@@ -2,7 +2,7 @@ const knex = require("knex");
 
 const db = knex({
   client: "pg",
-  connection: "postgresql://postgres:h1Q8pFvY1uRokM7f@db.nghssamptdjccacinnuy.supabase.co:5432/postgres"
+  connection: "postgresql://postgres:h1Q8pFvY1uRokM7f@db.nghssamptdjccacinnuy.supabase.co:5432/postgres?ssl=false"
 });
 console.log("my connection",db.connection)
 
