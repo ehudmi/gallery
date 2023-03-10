71a0e6e65659e09871504ee38dc526f44c06a3b6
ehud <ehudmi12@gmail.com>
2023-03-10 14:24:45 +0200

    db_url

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ dotenv.config();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-// app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 // app.use(
 //   cors({
 //     credentials: true,
